55ea8e5dd658a087ad0179026fea6225a73a6f72
Zenky <bzenky@gmail.com>
2022-10-04 21:43:05 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Oct 2022 00:36:35 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
